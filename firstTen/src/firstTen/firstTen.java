85bb192d8c4747b4e1c8e092127bbb214857baa2
Stan <Stan@Bre.home>
2016-10-06 02:12:02 -0400

    partial implementation of new print

--- firstTen/src/firstTen/firstTen.java
+++ firstTen/src/firstTen/firstTen.java
@@ -10,7 +10,7 @@ public class firstTen {
 		System.out.println("the first ten divided by the 11th is " + populateArray(newArray,keyboard));
 		System.out.println("test 2");
 		System.out.println("hi");
-		
+		System.out.println("derp");
 	}
 	
 	public static double populateArray(double[] numbers,Scanner scan){
